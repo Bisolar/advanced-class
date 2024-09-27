85c79efad723b3e9640999bb672eb91881fca9f8
Abisola Aransiola <ladipoabisola@gmail.com>
2024-09-27 07:45:56 +0100

    changed some text

--- converter.py
+++ converter.py
@@ -1,6 +1,6 @@
 message = print("Currency Exchange options:\n1. GBP to NGN\n2. USD to NGN\n3. AUD to NGN\n4. EUR to NGN\n5. CAD to NGN\n6. JPY to NGN ")
-user_choice= int(input("Please choose an option from (1-6)\n>>"))
-value = int(input("Enter amount: "))
+user_choice= int(input("\nPlease choose an option from (1-6)\n>>"))
+value = int(input("\nEnter amount: "))
 
 class CurrencyEx:
     def __init__(self, value):
@@ -34,6 +34,6 @@ class CurrencyEx:
         else:
             print("The option you entered is invalid")
     
-d = CurrencyEx(value)
-e = d.converter()
-print(e)
+amount = CurrencyEx(value)
+new_amount= amount.converter()
+print(new_amount)
